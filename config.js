48bda9635022ffafc77cc0a563a9cd14048a682b
gegeturambar@gmail.com <gegeturambar@gmail.com>
2023-01-10 17:16:18 +0100

    test fix api

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const apiUrl = process.env.NODE_ENV === 'development' 
     ? 'http://localhost:3000/api' // development api
-    : 'https://next-auth-example-khaki.vercel.app/users/api'; // production api
+    : 'https://next-auth-example-khaki.vercel.app/api'; // production api
 
 export {
     apiUrl
